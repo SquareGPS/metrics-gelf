9c4079c1744f7c1e7024afc744867f376a3f10cd
Evgeniy Devyatykh <e.devyatyh@squaregps.com>
2020-10-09 11:42:19 +0500

    Don't send NaN gauge value

--- src/main/java/org/graylog/metrics/GelfReporter.java
+++ src/main/java/org/graylog/metrics/GelfReporter.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016 Graylog, Inc. (hello@graylog.com)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -545,13 +545,18 @@ public class GelfReporter extends ScheduledReporter {
     }
 
     private void sendGauge(long timestamp, String name, Gauge gauge) {
+        Object value = gauge.getValue();
+
+        if (value == null || (value instanceof Number && !Double.isFinite(((Number) value).doubleValue()))) {
+            return;
+        }
         final GelfMessageBuilder message = new GelfMessageBuilder("name=" + name + " type=GAUGE", source)
                 .timestamp(timestamp)
                 .level(level)
                 .additionalFields(additionalFields)
                 .additionalField("name", name)
                 .additionalField("type", "GAUGE")
-                .additionalField("value", gauge.getValue());
+                .additionalField("value", value);
 
         gelfTransport.trySend(message.build());
     }
